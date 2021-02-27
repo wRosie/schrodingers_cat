87b4df437dd9fa482404d0d08a6e68abe1ac5cba
Yiyun Wang <yw3487@barnard.edu>
2021-02-26 23:13:35 -0600

    Modifed led control

--- led.py
+++ led.py
@@ -2,7 +2,7 @@
 import time
 import board
 import neopixel
-import COVID19Py
+#import COVID19Py
 import random
 import math
 import statistics
@@ -11,10 +11,11 @@ import socket
 # time.sleep(20)
 
 # Get Covid information from API
-covid19 = COVID19Py.COVID19()
-latest = covid19.getLatest()
+#covid19 = COVID19Py.COVID19()
+#latest = covid19.getLatest()
 
-cases = latest['confirmed']
+#cases = latest['confirmed']
+cases = 28500000
 print(cases)
 infected = False
 
@@ -36,22 +37,6 @@ pixels = neopixel.NeoPixel(
 # WIFI configurations
 LOCAL_UDP_IP = "192.168.1.2"
 SHARED_UDP_PORT = 4210
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
-sock.bind((LOCAL_UDP_IP, SHARED_UDP_PORT))
-
-
-def sigmoid(x):
-  return 1 / (1 + math.exp(-x))
-
-
-def natural_infection():
-    new_input = random.randint(0, cases)
-    rand_input.append(new_input)
-    if len(rand_input) > 10 :
-        rand_input.pop(0)
-    map(sigmoid, rand_input)
-    res = statistics.median(new_input)
-    return res
 
 
 # Scrodinger's parameters
@@ -61,6 +46,20 @@ digits = []
 cases_by_million = int(cases/1000000)
 # print(cases_by_million)
 
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
+
+
+def natural_infection():
+    new_input = random.randint(0, 500)
+    rand_input.append(new_input)
+    if len(rand_input) > 10 :
+        rand_input.pop(0)
+    map(sigmoid, rand_input)
+    res = statistics.median(rand_input)
+    return res
+
+
 while cases_by_million > 0:
     d = cases_by_million % 2
     digits.append(d)
@@ -68,16 +67,41 @@ while cases_by_million > 0:
 digits.reverse()
 # print(digits)
 
-# Update LED lights
-while True:
-    pixels.fill((0, 0, 0))
-    pixels.show()
-    time.sleep(1)
+
+
+if __name__ == "__main__":
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
+    sock.bind((LOCAL_UDP_IP, SHARED_UDP_PORT))
+    timer = 0
+    infection_time = natural_infection()
+    print(infection_time)
+    infected = False
+    light_color = (0,255,0)
+    # Update LED lights
     for i in range(len(digits)):
-        if digits[i] == 1:
-            pixels[i] = (255, 0, 0)
+        pixels[i] = (0,0,0)
     pixels.show()
-    time.sleep(1)
+    while True:
+        data, addr = sock.recvfrom(2048)
+        vals = data.decode().split(',')
+        for val in vals:
+            print(val)
+        x_in = vals[0]
+        y_in = vals[1]
+        z_in = vals[2]
+        b_in = vals[3]
+        
+        
+        if timer >= infection_time:
+            infected = True
+            light_color = (255,0,0)
+        for i in range(len(digits)):
+            if digits[i] == 1:
+                pixels[i] = light_color
+        pixels.show()
+        
+        time.sleep(1)
+        timer = timer+1
 
 
 
